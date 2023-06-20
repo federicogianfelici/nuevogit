82d460a0c3d0810d7c636a90c1ee86298875eda4
federicogianfelici <federicogianfelici@gmail.com>
2023-06-20 16:23:26 -0300

    first commit

---
+++ js/proyecto.js
@@ -0,0 +1,24 @@
+function cambiarfondo()
+    {document.body.style.backgroundColor= "black";
+    document.getElementById("aside").style.backgroundColor = "black";
+    document.getElementById("textouno").style.color = "white";
+    document.getElementById("titulouno").style.color = "white";
+    document.getElementById("aside").style.color = "white";
+    document.getElementById("titulodos").style.color = "white";
+    document.getElementById("titulotres").style.color = "white";
+    document.getElementById("titulocuatro").style.color = "white";
+    document.getElementById("botoncard").style.color = "white";
+    document.getElementById("botoncarduno").style.color = "white";
+    document.getElementById("botoncarddos").style.color = "white";
+    document.getElementById("botoncardtres").style.color = "white";
+    document.getElementById("botoncard").style.backgroundColor = "black";
+    document.getElementById("botoncarduno").style.backgroundColor = "black";
+    document.getElementById("botoncarddos").style.backgroundColor = "black";
+    document.getElementById("botoncardtres").style.backgroundColor = "black";
+    }
+
+function contacto()
+   {alert("Creador y diseñador: Federico Gianfelici. E-mail: federicogianfelici@gmail.com Teléfono:2214117657");
+    }
+    
+   
